ad5c8bda686dcd61c0eed51f5bd5f88761174c5b
sun zhuo <sunzhuo1228@126.com>
2015-09-10 01:03:14 +0800

    revert to Android Studio

--- BXJ/src/com/bxj/domain/WebData.java
+++
@@ -1,9 +0,0 @@
-package com.bxj.domain;
-
-import java.io.Serializable;
-
-public abstract class WebData implements Serializable {
-	public abstract String getUrl();
-
-	public abstract String getUrlType();
-}
